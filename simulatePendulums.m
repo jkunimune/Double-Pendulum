6d2a9e3cb45bbdb9e717ee1b8fecbb8ae9f60c57
Galactic Ketchup <jkunimune15@punahou.edu>
2015-11-12 12:06:15 -0500

    Double Pendulum
    We added the second one, but it's not chaotic yet, because we have not
    accounted for one force yet...

--- simulatePendulums.m
+++ simulatePendulums.m
@@ -6,19 +6,26 @@ lengthA = 1;%m
 massA = 1;%kg
 initThetaA = -pi/2;
 initVelA = 0;
+lengthB = 1;%m
+massB = 2;%kg
+initThetaB = -pi/2;
+initVelB = 0;
 
-ode45(@derivatives, [0,10], [initThetaA;initVelA]);
-legend('Theta_A','Velocity_A');
+ode45(@derivatives, [0,10], [initThetaA;initVelA;initThetaB;initVelB]);
+legend('Theta_A','Velocity_A','Theta_B','Velocity_B');
 
     function dYdt = derivatives(~,Y)
         thetaA = Y(1);
         velA = Y(2);
+        thetaB = Y(3);
+        velB = Y(4);
         
-        Fg = -massA*g;
+        FgA = -massA*g;
+        FgB = -massB*g;
         
-        accelA = (Fg*sin(thetaA))/(massA*lengthA);
+        accelA = (FgA*sin(thetaA))/(massA*lengthA);
+        accelB = (FgB*sin(thetaB))/(massB*lengthB);
         
-        dYdt = [velA; accelA];
+        dYdt = [velA; accelA; velB; accelB];
     end
 end
-
